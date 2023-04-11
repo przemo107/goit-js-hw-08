a127ea2d08a70deac07fd8fb01b5ac2b37853379
Przemysław Soliński <solinskip107@gmail.com>
2023-04-11 12:24:25 +0200

    hw-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Vimeo('vimeo-player');
+
+const saveCurrentTime = throttle(currentTime => {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000);
+
+player.on('timeupdate', ({ seconds }) => {
+  saveCurrentTime(seconds);
+});
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const saveFormState = () => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+};
+
+const loadFormState = () => {
+  const formState =
+    JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+  emailInput.value = formState.email || '';
+  messageInput.value = formState.message || '';
+};
+
+const clearFormState = () => {
+  const formState =
+    JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+  console.log('Form data cleared:', formState);
+};
+
+emailInput.addEventListener('input', throttle(saveFormState, 500));
+messageInput.addEventListener('input', throttle(saveFormState, 500));
+
+document.addEventListener('DOMContentLoaded', loadFormState);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  clearFormState();
+});
